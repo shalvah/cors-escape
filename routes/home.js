f984714edbb2c3b54c55108b7060e92b110b5fb1
shalvah <diakon.ng@gmail.com>
2019-06-01 08:51:27 +0200

    Fix help file

--- routes/home.js
+++ routes/home.js
@@ -1,25 +1,11 @@
 const helpText = {};
 
 function showHomePage(headers, response) {
-    const helpFile = require('path').join(__dirname, '/../lib/help.txt');
     headers['content-type'] = 'text/plain';
 
-    if (helpText[helpFile]) {
-        response.writeHead(200, headers);
-        response.end(helpText[helpFile]);
-    } else {
-        require('fs').readFile(helpFile, 'utf8', (err, data) => {
-            if (err) {
-                console.error(err);
-                response.writeHead(500, headers);
-                response.end();
-            } else {
-                // cCche contents of file
-                helpText[helpFile] = data;
-                showHomePage(headers, response); // Recursive call, but since data is a string, the recursion will end
-            }
-        });
-    }
+    const helpText = require('../lib/help');
+    response.writeHead(200, headers);
+    response.end(helpText);
 }
 
 module.exports = {
